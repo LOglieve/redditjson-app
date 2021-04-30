2a31bafb6a77865dc3623bb38f9484233caadc9d
Lily <lilymoglievechan@gmail.com>
2021-04-30 14:10:35 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="App">
+
+      <input type="text" className= "input" value ="Ducks"></input>
+      <div className = "articles">
+
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
