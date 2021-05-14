be5b7320a32f82bc08db9ae42657008df0a8021a
Lily <lilymoglievechan@gmail.com>
2021-05-14 12:52:54 +0100

    logo redirects to homescreen

--- src/components/support/nav.js
+++ src/components/support/nav.js
@@ -1,10 +1,16 @@
 import React, {useState, useEffect} from 'react';
+import { useHistory } from "react-router-dom";
 
 export default function Nav(props){
+    let history = useHistory();
+
+    const home = () => {
+        history.push("/home");
+    }
 
     return(
         <div className = "navBar">
-            <img src = "../resources/logo" id = "logo" alt = "logo" />
+            <img onClick = {home} src = "../resources/logo" id = "logo" alt = "logo" />
             <input id = "searchBar" type="text" className= "input" value = {props.subreddit} onKeyPress= {props.handleEnter} onChange = {props.handleChange}></input>
             
         </div>
